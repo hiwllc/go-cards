cbb61bf23e2450b56582f2d49ca75535ad303234
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-30 16:13:00 -0300

    :rainbow: variaveis

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  // basic types are: bool, string, int and float64
-  // var card string = "Ace of spades" or
-  // we only use := when declaration a new variable, when reassign just use equals
+  // basic types are: bool, string, int and float 64
+  // variables can be assign as var card string = "Ace of spades" or
   card := "Ace of spades"
+  // we only use := when declaration a new variable, when reassign just use equals
 
   fmt.Println(card)
 }
