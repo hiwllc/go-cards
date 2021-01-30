cd719decad3fcd2ab8ca5ceab449a1df67ef816f
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-30 16:10:37 -0300

    chore: initial settings

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  // basic types are: bool, string, int and float64
+  // var card string = "Ace of spades" or
+  // we only use := when declaration a new variable, when reassign just use equals
+  card := "Ace of spades"
+
+  fmt.Println(card)
+}
